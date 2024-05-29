46f67b696d8bbf8d1775248512d7a3f1c32ed115
ⵣΨ <azan3t@gmail.com>
2024-05-29 12:28:51 +0200

    Update bopscrk.py

--- bopscrk/bopscrk.py
+++ bopscrk/bopscrk.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-# https://github.com/r3nt0n/bopscrk
+# ORIGINAL: https://github.com/r3nt0n/bopscrk
+# AZANET MOD: https://github.com/r3nt0n/azanet/FORK-bopscrk_MOD
 # bopscrk - init script
 
 
 name = 'bopscrk.py'
 desc = 'Generate smart and powerful wordlists'
 __version__ = '2.4.5'
-__author__ = 'r3nt0n'
+__author__ = '"Mod by Azanet" -- Original Author: r3nt0n'
 __status__ = 'Development'
 
 
